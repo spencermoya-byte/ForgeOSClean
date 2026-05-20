db30e4c7f2885a3540bab1fd854addac0e8e4997
spencermoya-byte <spencermoya@icloud.com>
2026-05-19 21:10:58 -0700

    Import commits layout stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './App.css';
 import './workspaceSticky.css';
+import './commitsLayout.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
