c27aa5be4c39e68c221dbebbcde0ad4ba3cd4d88
spencermoya-byte <spencermoya@icloud.com>
2026-05-24 10:42:21 -0700

    Wire plugin manager into startup

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,19 @@ import './App.css';
 import './workspaceSticky.css';
 import './commitsLayout.css';
 import './executionPolicyStyles.css';
+import './pluginManager.css';
 import { AppErrorBoundary } from './appErrorBoundary';
 import { startLivePreviewInstaller } from './livePreviewInstaller';
 import { startTerminalWorkflowInstaller } from './terminalWorkflowInstaller';
 import { startLocalBuilderInstaller } from './localBuilderInstaller';
 import { startExecutionPolicyInstaller } from './executionPolicyInstaller';
+import { startPluginManagerInstaller } from './pluginManagerInstaller';
 
 startLivePreviewInstaller();
 startTerminalWorkflowInstaller();
 startLocalBuilderInstaller();
 startExecutionPolicyInstaller();
+startPluginManagerInstaller();
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
