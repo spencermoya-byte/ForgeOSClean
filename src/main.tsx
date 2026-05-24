3aa97df6f065dd9048da5cca1cd1f27519043d7f
spencermoya-byte <spencermoya@icloud.com>
2026-05-24 10:19:52 -0700

    Start execution policy installer

--- src/main.tsx
+++ src/main.tsx
@@ -8,14 +8,14 @@ import { AppErrorBoundary } from './appErrorBoundary';
 import { startLivePreviewInstaller } from './livePreviewInstaller';
 import { startTerminalWorkflowInstaller } from './terminalWorkflowInstaller';
 import { startLocalBuilderInstaller } from './localBuilderInstaller';
+import { startExecutionPolicyInstaller } from './executionPolicyInstaller';
 
 startLivePreviewInstaller();
 startTerminalWorkflowInstaller();
 startLocalBuilderInstaller();
+startExecutionPolicyInstaller();
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <AppErrorBoundary>
