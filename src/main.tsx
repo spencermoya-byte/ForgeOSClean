a071c1db06274870ca3fe9ff2468ae7b536a5548
spencermoya-byte <spencermoya@icloud.com>
2026-05-24 10:31:52 -0700

    Load execution policy workflow styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import './App.css';
 import './workspaceSticky.css';
 import './commitsLayout.css';
+import './executionPolicyStyles.css';
 import { AppErrorBoundary } from './appErrorBoundary';
 import { startLivePreviewInstaller } from './livePreviewInstaller';
 import { startTerminalWorkflowInstaller } from './terminalWorkflowInstaller';
