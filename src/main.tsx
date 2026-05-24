b5928f5969e2892738feb3b34d6dc21474a4a9db
spencermoya-byte <spencermoya@icloud.com>
2026-05-24 13:17:59 -0700

    Scope plugin UI to workspace utility rail

--- src/main.tsx
+++ src/main.tsx
@@ -5,24 +5,18 @@ import './App.css';
 import './workspaceSticky.css';
 import './commitsLayout.css';
 import './executionPolicyStyles.css';
-import './pluginManager.css';
-import './pluginDock.css';
 import './workspaceUtilityRail.css';
 import { AppErrorBoundary } from './appErrorBoundary';
 import { startLivePreviewInstaller } from './livePreviewInstaller';
 import { startTerminalWorkflowInstaller } from './terminalWorkflowInstaller';
 import { startLocalBuilderInstaller } from './localBuilderInstaller';
 import { startExecutionPolicyInstaller } from './executionPolicyInstaller';
-import { startPluginManagerInstaller } from './pluginManagerInstaller';
-import { startPluginDockInstaller } from './pluginDockInstaller';
 import { startWorkspaceUtilityRail } from './workspaceUtilityRail';
 
 startLivePreviewInstaller();
 startTerminalWorkflowInstaller();
 startLocalBuilderInstaller();
 startExecutionPolicyInstaller();
-startPluginManagerInstaller();
-startPluginDockInstaller();
 startWorkspaceUtilityRail();
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
