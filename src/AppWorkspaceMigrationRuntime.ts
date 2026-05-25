f5456b3257baf5d2566ca6dba958039f08b5aa1e
spencermoya-byte <spencermoya@icloud.com>
2026-05-24 18:46:41 -0700

    Alias migration runtime to workspace app runtime

--- src/AppWorkspaceMigrationRuntime.ts
+++ src/AppWorkspaceMigrationRuntime.ts
@@ -1,13 +1,1 @@
-import { useWorkspaceOwnedProjects } from "./AppWorkspaceOwnership";
-
-export function useAppWorkspaceMigrationRuntime() {
-  const workspace = useWorkspaceOwnedProjects();
-
-  return {
-    projects: workspace.projects,
-    activeProject: workspace.activeProject,
-    activeProjectId: workspace.activeProjectId,
-    hasProjects: workspace.hasProjects,
-    isWorkspaceOwned: true,
-  };
-}
+export { useWorkspaceAppRuntime as useAppWorkspaceMigrationRuntime } from "./workspaceAppRuntime";
